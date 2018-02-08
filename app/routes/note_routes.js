735ee8a507b5e098294904e5e49c247bb032c217
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-02-08 11:37:46 -0500

    added put and delete options

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -15,6 +15,35 @@ module.exports = function(app, client){
         });
     });
 
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id
+        const details = { '_id': new ObjectID(id) }
+        const db = client.db('nodejs-api')
+        db.collection('notes').remove(details, (err, item) => {
+            if(err){
+                res.send({ 'error': 'An error has occurred '})
+                client.close()
+            } else {
+                res.send('Note ' + id + ' deleted!' );
+            }
+        });
+    });
+
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id
+        const details = { '_id': new ObjectID(id) };
+        const note = { text: req.body.body, title: req.body.title }
+        const db = client.db('nodejs-api')
+        db.collection('notes').update(details, note, (err, item) => {
+            if(err){
+                res.send({ 'error': 'An error has occurred '})
+                client.close()
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
     app.post('/notes', (req, res) => {
         const db = client.db('nodejs-api')
         const note = { text: req.body.body, title: req.body.title }
