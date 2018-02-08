6664c05142cd2539e3005858d4f5988069c28249
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-02-08 11:30:18 -0500

    can now get information from the mongo database by using the get request.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,22 @@
+var ObjectID = require('mongodb').ObjectID
+
 module.exports = function(app, client){
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id
+        const details = { '_id': new ObjectID(id) }
+        const db = client.db('nodejs-api')
+        db.collection('notes').findOne(details, (err, item) => {
+            if(err){
+                res.send({ 'error': 'An error has occurred '})
+                client.close()
+            } else {
+                res.send(item);
+            }
+        });
+    });
 
     app.post('/notes', (req, res) => {
-        var db = client.db('nodejs-api')
+        const db = client.db('nodejs-api')
         const note = { text: req.body.body, title: req.body.title }
         db.collection('notes').insert(note, (err, result) => {
             if(err){
